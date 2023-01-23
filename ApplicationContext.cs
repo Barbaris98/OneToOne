2ce5ed6c2bcadb72971ed9431a9ad7d8322af29a
Dmitry Grigorev <d-d12@mail.ru>
2023-01-23 20:55:33 +0400

    Добавьте файлы проекта.

---
+++ ApplicationContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace OneToOne
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Login { get; set; }
+        public string? Password { get; set; }
+
+        public UserProfile? Profile { get; set; }
+    }
+
+    public class UserProfile
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+        public int Age { get; set; }
+
+        public int UserId { get; set; }
+        public User? User { get; set; }
+    }
+
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; } = null!;
+        public DbSet<UserProfile> UserProfiles { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=helloapp.db");
+        }
+    }
+
+
+
+}
